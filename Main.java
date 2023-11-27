8214152d4feba887aafc2c6ebcc93dc0061a46dc
jacob.estrich <jacob.s.estrich@gmail.com>
2023-11-27 15:32:49 -0800

    Edit: More changes

--- Main.java
+++ Main.java
@@ -6,10 +6,10 @@ public class Main
     {
         
         //Initialize the JFrame. 
-        JFrame window = new JFrame("GUI Test1 - Main file");
-        window.setSize(500,500);
-        window.setLocation(500, 300);
-        window.setVisible(true);             
+        // JFrame window = new JFrame("GUI Test1 - Main file");
+        // window.setSize(500,500);
+        // window.setLocation(500, 300);
+        // window.setVisible(true);             
         
         
         TicTacToe ticTacToe = new TicTacToe();
--- TicTacToe.java
+++ TicTacToe.java
@@ -1,63 +1,114 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
-
-
-
-public class TicTacToe extends TestJFrame
-{
-    ArrayList<JButton> buttons_array = new ArrayList<JButton>(); //array list called buttons
+public class TicTacToe extends TestJFrame {
+    
+    ArrayList<JButton> buttons = new ArrayList<JButton>();
     String currentPlayer;
 
-
-
-    public TicTacToe() 
-    {
+    public TicTacToe() {
         super();
 
         panel.setLayout(new GridLayout(3, 3));
+        Font font = new Font("SansSerif", Font.BOLD, 70);
 
-        for(int i = 0; i < 9; i++)
-        {
+        for(int i = 0; i < 9; i++){
             JButton button = new JButton();
             button.addActionListener(e -> ButtonClicked(e));
-
-            panel.add(button); //add button to the panel
-            buttons_array.add(button); //add these buttons to the buttons array list
+            button.setFont(font);
+            //button.setText("" + i);
+            panel.add(button);
+            buttons.add(button);
         }
-        setContentPane(panel); //have to set the content pane after every modification
 
+        setContentPane(panel);
     }
 
-    private void ButtonClicked(ActionEvent e)
-    {
-        switchPlayer();
+    private void ButtonClicked(ActionEvent e){
+        SwitchPlayer();
         JButton button = (JButton)e.getSource();
         button.setText(currentPlayer);
         button.setEnabled(false);
-    }
 
-    private void switchPlayer()
+
+        if(currentPlayer == "x"){
+            button.setBackground(Color.orange);
+        } else {
+            button.setBackground(new Color(100, 32, 200));
+        }
+
+        if(WeHaveAWinner()){
+            JOptionPane.showMessageDialog(null, currentPlayer +" has won!");
+
+        for(int i = 0; i < 9; i++){
+            buttons.get(i).setEnabled(false);
+        }
+    }
+    }
+    
+    private void SwitchPlayer() 
     {
-        if(currentPlayer == "X")
-        {
-            currentPlayer = "O";
-        }
-        else 
-        {
-            currentPlayer = "X";
+        if(currentPlayer == "x"){
+            currentPlayer = "o";
+        } else {
+            currentPlayer = "x";
         }
     }
 
+    private boolean WeHaveAWinner() {
+        if(buttons.get(0).getText().equals(currentPlayer) &&
+        buttons.get(1).getText().equals(currentPlayer) &&
+        buttons.get(2).getText().equals(currentPlayer)){
+            return true;
+        }
 
+        if(buttons.get(3).getText().equals(currentPlayer) &&
+        buttons.get(4).getText().equals(currentPlayer) &&
+        buttons.get(5).getText().equals(currentPlayer)){
+            return true;
+        }
 
+        if(buttons.get(6).getText().equals(currentPlayer) &&
+        buttons.get(7).getText().equals(currentPlayer) &&
+        buttons.get(8).getText().equals(currentPlayer)){
+            return true;
+        }
 
+    if(buttons.get(0).getText().equals(currentPlayer) &&
+        buttons.get(3).getText().equals(currentPlayer) &&
+        buttons.get(6).getText().equals(currentPlayer)){
+            return true;
+        }
 
+            if(buttons.get(1).getText().equals(currentPlayer) &&
+        buttons.get(4).getText().equals(currentPlayer) &&
+        buttons.get(7).getText().equals(currentPlayer)){
+            return true;
+        }
 
+        if(buttons.get(2).getText().equals(currentPlayer) &&
+        buttons.get(5).getText().equals(currentPlayer) &&
+        buttons.get(8).getText().equals(currentPlayer)){
+            return true;
+        }
 
+        if(buttons.get(0).getText().equals(currentPlayer) &&
+        buttons.get(4).getText().equals(currentPlayer) &&
+        buttons.get(8).getText().equals(currentPlayer)){
+            return true;
+        }
 
+        if(buttons.get(2).getText().equals(currentPlayer) &&
+        buttons.get(4).getText().equals(currentPlayer) &&
+        buttons.get(6).getText().equals(currentPlayer)){
+            return true;
+        }
 
-
+        return false;
+    }
 }
